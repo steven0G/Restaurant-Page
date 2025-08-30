265dfa2a1907583807406a298b13b2e0fc14eb95
steven0G <stevengabriel2048@gmail.com>
2025-08-30 13:47:25 +0700

    Add new Section

--- src/home.js
+++ src/home.js
@@ -1,13 +1,22 @@
+import "./styles/home.css";
 import { MenuPage } from "./menu";
 
 
 function HomePage() {
     let content = document.querySelector("#content");
+    let homeSection = document.createElement("div");
+    homeSection.setAttribute("class", "home-section");
+
     let restaurantName = document.createElement("h1");
+    restaurantName.setAttribute("class", "restaurant-heading");
     let summary = document.createElement("p");
+    summary.setAttribute("class", "home-summary");
+
     let orderButton = document.createElement("button");
+    orderButton.setAttribute("class", "order-btn");
 
     orderButton.addEventListener("click",()=> {
+      content.textContent = "";
       MenuPage();
     });
     
@@ -15,11 +24,13 @@ function HomePage() {
     summary.textContent = "Bold Cuts. Rich Flavor. Timeless Fire.";
     orderButton.textContent = "Order Now!";
 
-    content.appendChild(restaurantName);
-    content.appendChild(summary);
-    content.appendChild(orderButton);
+    content.appendChild(homeSection);
 
+    homeSection.appendChild(restaurantName);
+    homeSection.appendChild(summary);
+    homeSection.appendChild(orderButton);
 
+   
 }
 
 export { HomePage };
